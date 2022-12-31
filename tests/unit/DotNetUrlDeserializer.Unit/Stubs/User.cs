7499a67c409562f66b3f24eb7191f33fe70155b3
Florent Bunjaku <fb48234@ubt-uni.net>
2022-12-31 11:00:51 +0100

    unit tests

---
+++ tests/unit/DotNetUrlDeserializer.Unit/Stubs/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetUrlDeserializer.Unit.Stubs
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; }
+        public bool Verified { get; set; }
+        public List<User> Friends { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class Address
+    {
+        public Country Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
